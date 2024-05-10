307b559bb94c60c3ca81ed4da63bdcb56407e469
nohiup <nohiup@gmail.com>
2024-05-10 21:33:14 +0700

    f

--- app/src/main/java/com/softwaredesign/novelreader/DetailActivity.java
+++ app/src/main/java/com/softwaredesign/novelreader/DetailActivity.java
@@ -2,10 +2,17 @@ package com.softwaredesign.novelreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.softwaredesign.novelreader.Models.NovelModel;
+import com.softwaredesign.novelreader.SourceHandler.TruyenfullParser;
 import com.squareup.picasso.Picasso;
 
 import org.jsoup.Jsoup;
@@ -20,6 +27,8 @@ public class DetailActivity extends AppCompatActivity {
     private ImageView detailImage;
     private TextView detailName, detailAuthor, detailDescription;
     private String NovelUrl;
+    private TruyenfullParser truyenfullParser = new TruyenfullParser();
+    private String data[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +40,51 @@ public class DetailActivity extends AppCompatActivity {
         detailAuthor = findViewById(R.id.detailAuthor);
         detailDescription = findViewById(R.id.detailDescription);
 
+
         // Get novelUrl from selected novel
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
+
             NovelUrl = bundle.getString("NovelUrl");
+
+            NovelUrl = NovelUrl.replace("chuong-1/", "");
+            Log.d("Url", NovelUrl.toString());
+
+            DetailActivity.Content content = new DetailActivity.Content();
+            content.execute();
+//            Log.d("data", data[0] + " - " + data[1] + data[2] + data[3]);
+//            detailName.setText(data[0]);
+//            detailAuthor.setText(data[1]);
+//            detailDescription.setText(data[2]);
+//            Picasso.get().load(data[3]).placeholder(R.drawable.logo).into(detailImage);
+
         }
     }
+    private class Content extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            data = truyenfullParser.novelDetailScrapping(NovelUrl);
+            return null;
+        }
+
+}
+
+
 }
---
+++ app/src/main/java/com/softwaredesign/novelreader/SourceHandler/TruyenfullParser.java
@@ -0,0 +1,121 @@
+package com.softwaredesign.novelreader.SourceHandler;
+
+import android.util.Log;
+
+import com.softwaredesign.novelreader.Factory.NovelScrapperFactory;
+import com.softwaredesign.novelreader.Models.NovelModel;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class TruyenfullParser implements NovelScrapperFactory{
+    public String SEARCH_DEFAULT_URL = "https://truyenfull.vn/tim-kiem/?tukhoa=";
+    public String ITEM_TYPE = "https://schema.org/Book";
+    @Override
+    public ArrayList<NovelModel> searchPageScrapping(String keyword) {
+        String searchUrl = this.SEARCH_DEFAULT_URL + keyword;
+        ArrayList<NovelModel> novelList = new ArrayList<>();
+
+        try {
+            Document doc = Jsoup.connect(searchUrl).get();
+            Elements rowNodes = doc.select("div.row");
+
+            for (Element row: rowNodes){
+                String itemScope = row.attr("itemtype");
+                if (itemScope.equals(this.ITEM_TYPE)){
+
+                    String novelName = getNovelNameInRowNode(row);
+                    String novelHref = getNovelLinkInRowNode(row);
+                    String novelAuthor = getNovelAuthorInRowNode(row);
+                    String novelThumbnailsUrl = getThumbnailsUrlInRowNode(row);
+
+                    NovelModel novel = new NovelModel(novelName, novelHref, novelAuthor, novelThumbnailsUrl);
+                    novelList.add(novel);
+                    Log.d("Novel data", novel.toString());
+                }
+            }
+            return novelList;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String[] novelDetailScrapping(String url) {
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Element headerNode = doc.getElementById("truyen");
+//            Log.d("headerNode", headerNode.toString());
+            String novelName = headerNode.selectFirst("h3.title").text();
+            String novelAuthor = headerNode.selectFirst("a[itemprop=author]").text();
+            Elements novelDescriptionNode = headerNode.select("div[itemprop=description]").select("br");
+            String novelDeskImgUrl = headerNode.selectFirst("img[itemprop=image]").attr("src");
+            String novelDescription = "";
+            Log.d("text", novelDescriptionNode.first().toString());
+            if (novelDescriptionNode!=null){
+                for (Element br: novelDescriptionNode){
+                    novelDescription += br.text();
+
+                    novelDescription += "\n";
+                }
+            }
+
+            String data[] = new String[4];
+
+            data[0] = novelName;
+            data[1] = novelAuthor;
+            data[2] = novelDescription;
+            data[3] = novelDeskImgUrl;
+
+
+            return data;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void novelChapterListScrapping(String url) {
+
+    }
+
+    //Novel list support methods
+    private String getNovelLinkInRowNode(Element row){
+        Element linkNode = row.select("a[href]").first();
+        if (linkNode != null)
+        {
+            return linkNode.attr("href");
+        }
+        return null;
+    }
+    private String getNovelAuthorInRowNode(Element row){
+        Element authorNode = row.select("span.author").first();
+        if (authorNode != null){
+            return authorNode.text();
+        }
+        return null;
+    }
+    private String getNovelNameInRowNode(Element row){
+        Element nameNode = row.select("h3.truyen-title").first();
+        if (nameNode!=null){
+            return nameNode.text();
+        }
+        else return null;
+    }
+    private String getThumbnailsUrlInRowNode(Element row){
+        Element imgNode = row.select("div[data-image]").first();
+
+        if (imgNode!=null){
+            String data = imgNode.attr("data-desk-image");
+            return imgNode.attr("data-desk-image");
+        }
+        return null;
+    }
+
+    //Novel description support methods
+}
