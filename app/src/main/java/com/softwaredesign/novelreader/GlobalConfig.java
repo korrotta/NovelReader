3e1dc0338977278f88b069afdd96e76c2c7f98d0
nohiup <nohiup@gmail.com>
2024-06-07 13:12:16 +0700

    Update chapter list base on currentPage and pagesize of each source

--- app/src/main/java/com/softwaredesign/novelreader/GlobalConfig.java
+++ app/src/main/java/com/softwaredesign/novelreader/GlobalConfig.java
@@ -1,10 +1,21 @@
 package com.softwaredesign.novelreader;
 
+import android.util.Log;
+
 import com.softwaredesign.novelreader.Models.NovelSourceModel;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class GlobalConfig {
     public static List<NovelSourceModel> g_SourceList;
+    public static void logVariable(Object var){
+        Class varClass = var.getClass();
+        Field[] fields = varClass.getFields();
+        for (Field field: fields){
+            Log.d(field.getName() + " value check", var.toString());
+        }
 
+
+    }
 }
--- novelscraperfactory/src/main/java/com/example/novelscraperfactory/NovelScraperFactory.java
+++ novelscraperfactory/src/main/java/com/example/novelscraperfactory/NovelScraperFactory.java
@@ -14,5 +14,7 @@ public interface NovelScraperFactory {
 
     <T>List<T> novelHomePageScraping(String url);
 
+    int getNumberOfChaptersPerPage();
+
 
 }
